d082b65ac67fbf7f21d0161f1450696a91e71632
Dat184 <thanhnguyendat184@gmail.com>
2025-03-14 14:48:26 +0700

    add Arduino integration to rentals service and update environment configuration

--- src/modules/adruino/adruino.module.ts
+++ src/modules/adruino/adruino.module.ts
@@ -1,7 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AdruinoService } from './adruino.service';
+// import { AdruinoController } from './adruino.controller';
+import { SerialPort } from 'serialport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  providers: [AdruinoService],
+  imports: [ConfigModule],
+  // controllers: [AdruinoController],
+  providers: [
+    AdruinoService,
+    {
+      provide: 'SERIAL_PORT', // Sử dụng string token để tránh xung đột với class SerialPort
+      useFactory: (configService: ConfigService) => {
+        return new SerialPort({
+          path: configService.get<string>('ARDUINO_PORT') || 'COM5',
+          baudRate: 9600,
+        });
+      },
+      inject: [ConfigService],
+    },
+  ],
+  exports: [AdruinoService],
 })
 export class AdruinoModule {}
